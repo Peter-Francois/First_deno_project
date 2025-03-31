f49ad12cdb0741f9e4a11d45373b27ef156c321d
Peter Francois <peter.francois237@hotmail.com>
2025-03-31 07:14:14 +0000

    Mise à jour du cache Git pour prendre en compte le .gitignore

--- .deno/gen/file/home/runner/workspace/index.ts.js
+++
@@ -1,9 +0,0 @@
-import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
-const port = 3000;
-const app = new expressive.App();
-app.get("/", (_req, res)=>{
-  res.send("Hello from Replit\r\n");
-});
-const server = await app.listen(port, "0.0.0.0");
-console.log("app listening on port " + server.port);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZpbGU6Ly8vaG9tZS9ydW5uZXIvd29ya3NwYWNlL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGV4cHJlc3NpdmUgZnJvbSBcImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9OTWF0aGFyL2Rlbm8tZXhwcmVzcy9tYXN0ZXIvbW9kLnRzXCI7XG5cbmNvbnN0IHBvcnQgPSAzMDAwO1xuY29uc3QgYXBwID0gbmV3IGV4cHJlc3NpdmUuQXBwKCk7XG5hcHAuZ2V0KFwiL1wiLCAoX3JlcSwgcmVzKSA9PiB7XG4gIHJlcy5zZW5kKFwiSGVsbG8gZnJvbSBSZXBsaXRcXHJcXG5cIik7XG59KTtcbmNvbnN0IHNlcnZlciA9IGF3YWl0IGFwcC5saXN0ZW4ocG9ydCwgXCIwLjAuMC4wXCIpO1xuY29uc29sZS5sb2coXCJhcHAgbGlzdGVuaW5nIG9uIHBvcnQgXCIgKyBzZXJ2ZXIucG9ydCk7XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsWUFBWSxnQkFBZ0IsdUVBQXVFO0FBRW5HLE1BQU0sT0FBTztBQUNiLE1BQU0sTUFBTSxJQUFJLFdBQVcsR0FBRztBQUM5QixJQUFJLEdBQUcsQ0FBQyxLQUFLLENBQUMsTUFBTTtFQUNsQixJQUFJLElBQUksQ0FBQztBQUNYO0FBQ0EsTUFBTSxTQUFTLE1BQU0sSUFBSSxNQUFNLENBQUMsTUFBTTtBQUN0QyxRQUFRLEdBQUcsQ0FBQywyQkFBMkIsT0FBTyxJQUFJIn0=
--- main.ts
+++ main.ts
@@ -1,6 +1,34 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const PORT = 8000;
-console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+// Création du routeur
+const router = new Router();
+router.get("/", (context) => {
+  context.response.body = `
+    <html>
+      <head>
+        <title>Compteur en Deno</title>
+      </head>
+      <body>
+        <h1>Compteur : <span id="count">0</span></h1>
+        <button onclick="increment()">+1</button>
 
-serve((req) => new Response("Hello, API!"), { port: PORT });
+        <script>
+          function increment() {
+            let countElement = document.getElementById("count");
+            let count = parseInt(countElement.innerText);
+            countElement.innerText = count + 1;
+          }
+        </script>
+      </body>
+    </html>
+  `;
+});
+
+// Création de l'application
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// Démarrage du serveur sur le port 8000
+console.log("Serveur démarré sur http://localhost:8000");
+await app.listen({ port: 8000 });
